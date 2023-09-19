a3a47571580377a51fcfc528c260d06265103b7d
kuntalkumar <kuntalkumar789@gmail.com>
2023-09-19 23:39:26 +0530

    functionlity works fine

---
+++ razorpay/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Demo from './Component/CartComponent/Demo.jsx'
+// import Comp from './Component/Comp';
+// import RazorpayPayment from './Component/RazorpayPayment';
+function App() {
+  return (
+    <div className="App">
+        <Demo/>
+
+      {/* <AddressPage/> */}
+    </div>
+  );
+}
+
+export default App;
